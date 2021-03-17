1bc0673302a7ec2af2fcf37cd238801c14aa7827
Christian Müller <christian.muller@wifx.net>
2021-03-17 11:19:40 +0100

    State script UPGRADE_LOG_PERSISTANT_DIR type

--- meta/recipes-update/mender-state-scripts-base/files/logs-restore.sh
+++ meta/recipes-update/mender-state-scripts-base/files/logs-restore.sh
@@ -3,7 +3,7 @@
 PREFIX=RESTORE-LOGS
 source /data/mender/migration-env
 
-if [ -d "$UPGRADE_LOG_PERSISTANT_DIR" ]; then
+if [ -d "$UPGRADE_LOG_PERSISTENT_DIR" ]; then
     log $PREFIX "Restoring upgrade logs"
-    mv $UPGRADE_LOG_PERSISTANT_DIR/* "${UPGRADE_LOG_PATH%/*}"
+    mv $UPGRADE_LOG_PERSISTENT_DIR/* "${UPGRADE_LOG_PATH%/*}"
 fi
--- meta/recipes-update/mender-state-scripts-base/files/logs-save.sh
+++ meta/recipes-update/mender-state-scripts-base/files/logs-save.sh
@@ -4,8 +4,8 @@ PREFIX=SAVE-LOGS
 source /data/mender/migration-env
 
 if [ -f "$UPGRADE_LOG_PATH" ]; then
-    log $PREFIX "Saving '$UPGRADE_LOG_PATH' to '$UPGRADE_LOG_PERSISTANT_DIR'"
-    mkdir -p $UPGRADE_LOG_PERSISTANT_DIR
-    cp "$UPGRADE_LOG_PATH" $UPGRADE_LOG_PERSISTANT_DIR/
-    log $PREFIX "Logs have been saved to '$UPGRADE_LOG_PERSISTANT_DIR'"
+    log $PREFIX "Saving '$UPGRADE_LOG_PATH' to '$UPGRADE_LOG_PERSISTENT_DIR'"
+    mkdir -p $UPGRADE_LOG_PERSISTENT_DIR
+    cp "$UPGRADE_LOG_PATH" $UPGRADE_LOG_PERSISTENT_DIR/
+    log $PREFIX "Logs have been saved to '$UPGRADE_LOG_PERSISTENT_DIR'"
 fi
