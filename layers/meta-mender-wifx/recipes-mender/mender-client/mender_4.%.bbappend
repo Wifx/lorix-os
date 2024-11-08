70a47e6f1c5f0a9e0c0e0ea713f607ac9e703f05
Christian Müller <christian.muller@wifx.net>
2024-11-08 18:57:24 +0100

    [LOR-598] Provide a delta system update mechanism

--- layers/meta-mender-wifx/recipes-mender/mender-client/mender_4.%.bbappend
+++ layers/meta-mender-wifx/recipes-mender/mender-client/mender_4.%.bbappend
@@ -8,6 +8,7 @@ RPROVIDES:${PN} += "virtual/updater"
 FILESEXTRAPATHS:prepend := "${THISDIR}/files:"
 SRC_URI:append = " \
     file://artifact-verify-key.pem \
+    file://wifx-binary-delta \
     file://0001-configurable-root-device.patch \
     file://0002-force-ubiupdatevol-use.patch \
     file://0003-remove-unused-modules.patch \
@@ -21,9 +22,14 @@ MENDER_SERVER_URL = "https://hosted.mender.io"
 FILES:mender-update += "\
     ${datadir}/mender/inventory \
     ${datadir}/mender/inventory/mender-inventory-machine \
+    ${datadir}/mender/modules/v3 \
+    ${datadir}/mender/modules/v3/wifx-binary-delta \
 "
 
 do_install:append() {
     install -m 755 -d ${D}/${datadir}/mender/inventory
     ln -s /usr/sbin/machine-info ${D}/${datadir}/mender/inventory/mender-inventory-machine
+
+    install -m 755 -d ${D}/${datadir}/mender/modules/v3
+    install -m 755 ${WORKDIR}/wifx-binary-delta ${D}/${datadir}/mender/modules/v3/wifx-binary-delta
 }
---
+++ tools/build-system/generate-wifx-binary-delta.sh
@@ -0,0 +1,138 @@
+#!/bin/bash -e
+#
+# generate-wifx-binary-delta.sh
+#
+# This script generates a binary delta between two Mender artifacts.
+#
+# Usage:
+#   ./generate-wifx-binary-delta.sh <source-artifact> <target-artifact>
+#
+# Arguments:
+#   <source-artifact>  Path to the source Mender artifact.
+#   <target-artifact>  Path to the target Mender artifact.
+#
+# Description:
+#   This script takes two Mender artifacts as input and generates a binary delta
+#   between them.
+#   - The device types of the two artifacts must match. (The script will fail if they do not.)
+#   - State scripts (migrations) from the target artifact are automatically included in the delta artifact. 
+#
+# Exit Codes:
+#   1  If the script is called with incorrect arguments or if the artifacts are not found.
+#
+# Dependencies:
+#   This script requires the following tools to be installed:
+#   - jq: Command-line JSON processor.
+#   - tar: Archiving utility.
+#   - xdelta3: Binary delta generator.
+#
+# Example:
+#   ./generate-wifx-binary-delta.sh source.mender target.mender
+#
+# Author:
+#   Wifx SA <info@iot.wifx.net>
+#
+# License:
+#   All rights reserved.
+#
+
+SOURCE_ARTIFACT_PATH=$1
+TARGET_ARTIFACT_PATH=$2
+
+if [ -z "$SOURCE_ARTIFACT_PATH" ] || [ -z "$TARGET_ARTIFACT_PATH" ]; then
+    echo "Usage: $0 <source-artifact> <target-artifact>"
+    exit 1
+fi
+
+if [ ! -f $SOURCE_ARTIFACT_PATH ]; then
+    echo "Source artifact not found"
+    exit 1
+fi
+
+if [ ! -f $TARGET_ARTIFACT_PATH ]; then
+    echo "Target artifact not found"
+    exit 1
+fi
+
+SOURCE_DIR_NAME=$(basename $SOURCE_ARTIFACT_PATH .mender)
+TARGET_DIR_NAME=$(basename $TARGET_ARTIFACT_PATH .mender)
+
+get_device_types() {
+    local device_types=$(jq -r '.artifact_depends.device_type[]' $1/header/header-info)
+    device_types=($device_types)
+    echo "${device_types[@]}"
+}
+
+extract_artifact() {
+    artifact_path=$1
+    directory=$2
+    rm -rf $directory && mkdir $directory
+    tar -xf $artifact_path -C $directory
+    mkdir $directory/header && tar -xf $directory/header.tar.gz -C $directory/header
+    mkdir -p $directory/data/0000 && tar -xf $directory/data/0000.tar.gz -C $directory/data/0000
+}
+
+# Extract artifacts
+extract_artifact $SOURCE_ARTIFACT_PATH $SOURCE_DIR_NAME
+extract_artifact $TARGET_ARTIFACT_PATH $TARGET_DIR_NAME
+
+# Find device types for both artifacts
+source_device_types=($(get_device_types $SOURCE_DIR_NAME))
+target_device_types=($(get_device_types $TARGET_DIR_NAME))
+
+# Check that device types are the same for both artifacts
+if [ "${source_device_types[*]}" != "${target_device_types[*]}" ]; then
+    echo "Device types do not match"
+    exit 1
+fi
+
+device_types_args=""
+for device_type in "${target_device_types[@]}"; do
+    device_types_args="$device_types_args --device-type $device_type"
+done
+
+get_artifact_name() {
+    local artifact_name=$(jq -r '.artifact_provides.artifact_name' $1/header/header-info)
+    echo $artifact_name
+}
+
+source_artifact_name=($(get_artifact_name $SOURCE_DIR_NAME))
+target_artifact_name=($(get_artifact_name $TARGET_DIR_NAME))
+
+xdelta3 -e -f \
+    -s $SOURCE_DIR_NAME/data/0000/*.ubifs \
+    $TARGET_DIR_NAME/data/0000/*.ubifs \
+    delta.ubifs
+
+script_args=""
+# For all files in the target artifact script directory
+for script in $TARGET_DIR_NAME/header/scripts/*; do
+    script_args="$script_args --script $script"
+done
+
+# Get target artifact file size
+target_image_size=$(stat -c %s $TARGET_DIR_NAME/data/0000/*.ubifs)
+
+# Write meta-data file
+rm -f meta-data.json
+echo '{ "target_image_size": "'$target_image_size'" }' > meta-data.json
+
+
+delta_artifact_name="delta_${source_artifact_name}_${target_artifact_name}"
+
+# Add --device-type argument for each device type
+mender-artifact write module-image \
+    --type "wifx-binary-delta" \
+    $device_types_args \
+    $script_args \
+    --artifact-name $delta_artifact_name \
+    --file delta.ubifs \
+    --meta-data meta-data.json \
+    --output-path $delta_artifact_name.mender
+
+#extract_artifact $delta_artifact_name.mender $delta_artifact_name
+
+# Clean up
+rm -rf $SOURCE_DIR_NAME $TARGET_DIR_NAME delta.ubifs meta-data.json $delta_artifact_name
+
+echo "Done"
