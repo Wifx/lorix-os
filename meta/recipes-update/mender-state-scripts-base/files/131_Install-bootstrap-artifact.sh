3b16b0e18906bd5dd471ec7b2f9e325678c59b29
Christian Müller <christian.muller@wifx.net>
2025-01-13 16:58:16 +0100

    Fix mender state script logging

--- meta/recipes-update/mender-state-scripts-base/files/131_Install-bootstrap-artifact.sh
+++ meta/recipes-update/mender-state-scripts-base/files/131_Install-bootstrap-artifact.sh
@@ -5,11 +5,11 @@ error=false
 
 UBIDATA_MOUNT_PATH=/var/lib/migration/ubidata
 
-source /data/mender/upgrade/migration-env.sh || exit 1
+source /data/mender/upgrade/migration-env.sh
 
 # Mount ubidata volume
 
-echo "$PREFIX: Mounting ubidata volume on $UBIDATA_MOUNT_PATH"
+log $PREFIX "Mounting ubidata volume on $UBIDATA_MOUNT_PATH"
 
 mkdir -p $UBIDATA_MOUNT_PATH
 mount -t ubifs ubi0:data $UBIDATA_MOUNT_PATH
@@ -22,10 +22,10 @@ rm -f $BOOTSTRAP_ARTIFACT_PATH/*
 
 cp /tmp/mender/bootstrap-artifact/bootstrap.mender $BOOTSTRAP_ARTIFACT_PATH
 if [ $? -ne 0 ]; then
-    echo "$PREFIX: Failed to copy bootstrap artifact to $BOOTSTRAP_ARTIFACT_PATH"
+    log $PREFIX "Failed to copy bootstrap artifact to $BOOTSTRAP_ARTIFACT_PATH"
     error=true
 else
-    echo "$PREFIX: Copied bootstrap artifact to $BOOTSTRAP_ARTIFACT_PATH"
+    log $PREFIX "Copied bootstrap artifact to $BOOTSTRAP_ARTIFACT_PATH"
 fi
 
 # Cleanup
